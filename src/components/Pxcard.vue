7344cf52915e1c3e127c09f12672220e02aacbaf
trisnamaulida <60949474+TrisnaMaulida@users.noreply.github.com>
2022-01-09 22:05:38 +0700

    buang icon gak penting

--- src/components/Pxcard.vue
+++ src/components/Pxcard.vue
@@ -7,30 +7,33 @@
 
 <template>
   <div>
-    <b-card no-body :class="{ 'full-card' : fullCard}">
+    <b-card no-body :class="{ 'full-card': fullCard }">
       <template v-slot:header v-if="showHeader || this.$slots.headerCustom">
         <h5 v-if="title">{{ title }}</h5>
-        
+
         <!-- header actions slot -->
         <div v-if="showAction">
           <slot name="headerAction">
             <div class="card-header-right">
-              <ul class="list-unstyled card-option" :class="{ openaction : openActions}">
-                <li>
+              <ul
+                class="list-unstyled card-option"
+                :class="{ openaction: openActions }"
+              >
+                <!-- <li>
                   <i @click="openCardActions()" :class="openActions ? 'icofont icofont-double-right' : 'fa fa-spin fa-cog'"></i>
-                </li>
-                <li @click="openFullCard()">
+                </li> -->
+                <!-- <li @click="openFullCard()">
                   <i
                     class="icofont full-card"
                     :class="fullCard ? 'icofont-resize' : 'icofont-maximize'"
                   ></i>
-                </li>
-                <li @click="toggleCard()">
+                </li> -->
+                <!-- <li @click="toggleCard()">
                   <i
                     class="icofont minimize-card"
                     :class="cardToggle ? 'icofont-minus' : 'icofont-plus'"
                   ></i>
-                </li>
+                </li> -->
               </ul>
             </div>
           </slot>
@@ -44,12 +47,15 @@
       <b-card-body
         v-if="this.$slots.default"
         class="p-0"
-        :style="cardToggle ? {'display': 'block'} : {'display': 'none'} "
+        :style="cardToggle ? { display: 'block' } : { display: 'none' }"
       >
         <slot></slot>
       </b-card-body>
       <!-- card body with padding -->
-      <b-card-body v-else :style="cardToggle ? {'display': 'block'} : {'display': 'none'} ">
+      <b-card-body
+        v-else
+        :style="cardToggle ? { display: 'block' } : { display: 'none' }"
+      >
         <slot name="with-padding"></slot>
       </b-card-body>
 
@@ -61,7 +67,6 @@
       <!-- card custom content -->
       <slot name="custom-content"></slot>
     </b-card>
-    
   </div>
 </template>
 
@@ -73,13 +78,13 @@ export default {
     actions: {
       type: Boolean,
       default: true,
-    }
+    },
   },
   data() {
     return {
       openActions: false,
       fullCard: false,
-      cardToggle: true
+      cardToggle: true,
     };
   },
   computed: {
@@ -87,8 +92,8 @@ export default {
       return this.title;
     },
     showAction() {
-      return this.$slots.headerAction || this.actions
-    }
+      return this.$slots.headerAction || this.actions;
+    },
   },
   methods: {
     openCardActions() {
@@ -99,7 +104,7 @@ export default {
     },
     toggleCard() {
       this.cardToggle = !this.cardToggle;
-    }
-  }
+    },
+  },
 };
 </script>
